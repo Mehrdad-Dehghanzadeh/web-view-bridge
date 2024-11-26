94a71b595b1e5b1f66118c13b1c8189b335d0324
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2024-11-26 23:58:38 +0330

    sd

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,31 @@
 const path = require("path");
 
 const srcDir = path.resolve(__dirname, "src");
-const outputDir = path.resolve(__dirname, "output");
+const outputDir = path.resolve(__dirname, "dist");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
 
   entry: {
-    index: `${srcDir}/index.js`,
+    index: `${srcDir}/index.ts`,
   },
 
   output: {
     path: outputDir,
     filename: "[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  
+  resolve: {
+    extensions: [ ".ts", ".js"],
   },
 };
