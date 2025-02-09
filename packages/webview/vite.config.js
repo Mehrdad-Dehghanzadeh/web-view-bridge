64fc3f2f751908e243135fa34e8d3b337ea40f86
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2025-02-09 17:05:12 +0330

    feat: add build and imporve proforamce

--- packages/webview/vite.config.js
+++ packages/webview/vite.config.js
@@ -1,4 +1,8 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(() => {
   return {
@@ -6,9 +10,19 @@ export default defineConfig(() => {
     root: 'src',
 
     build: {
-      outDir: '../../../dist',
+      outDir: resolve(__dirname, '../../dist'),
       emptyOutDir: true,
-      target: ['firefox78', 'chrome80', 'safari12.3']
+      target: ['firefox78', 'chrome80', 'safari12.3'],
+
+      lib: {
+        entry: resolve(__dirname, './src/lib/main.ts'),
+        name: 'webViewBridge',
+        // the proper extensions will be added
+        fileName: () => `webViewBridge.js`,
+        formats: ['iife']
+      },
+
+      minify: true
     }
   }
 })
